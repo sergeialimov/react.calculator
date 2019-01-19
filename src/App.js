7979193736bf0dc527c5b674b7d7f4d4724ee415
Sergei Alimov <8912652@gmail.com>
2019-01-19 22:42:26 +0300

    add buttons in second line

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ class App extends Component {
           </div>
           <div id="firstRow">
             <div id="ac">AC</div>
-            <div id="divide"></div>
-            <div id="multiply"></div>
+            <div id="divide">/</div>
+            <div id="multiply">*</div>
           </div>
           <div id="secondRow">
+            <div id="7">7</div>
+            <div id="8">8</div>
+            <div id="9">9</div>
+            <div id="minus">-</div>
           </div>
           <div id="thirdRow">
           </div>
