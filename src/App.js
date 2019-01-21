15233869865408c7153cdce7fc3337ed511d8fe6
Sergei Alimov <8912652@gmail.com>
2019-01-21 09:26:46 +0300

    remove rows

--- src/App.js
+++ src/App.js
@@ -8,30 +8,19 @@ class App extends Component {
         <div id="calc">
           <div id="display">
           </div>
-          <div id="firstRow">
             <div id="ac">
               <p>AC</p>
             </div>
             <div id="divide"><p>/</p></div>
             <div id="multiply"><p>x</p></div>
-          </div>
-          <div id="secondRow">
             <div id="7"><p>7</p></div>
             <div id="8"><p>8</p></div>
             <div id="9"><p>9</p></div>
             <div id="minus"><p>–</p></div>
-          </div>
-          <div id="thirdRow">
             <div id="4"><p>4</p></div>
             <div id="5"><p>5</p></div>
             <div id="6"><p>6</p></div>
             <div id="plus"><p>+</p></div>
-          </div>
-          <div id="fourthRow">
-          </div>
-          <div id="fifthRow">
-          </div>
-          <div id="sixthRow">
           </div>
         </div>
       </div>
