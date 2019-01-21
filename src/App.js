96747de03b27585dcd8e9f93b8d23a088c613ab0
Sergei Alimov <8912652@gmail.com>
2019-01-21 09:31:11 +0300

    add display

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ class App extends Component {
         <div id="calc">
           <div id="display">
           </div>
-            <div id="ac">
-              <p>AC</p>
-            </div>
-            <div id="divide"><p>/</p></div>
-            <div id="multiply"><p>x</p></div>
-            <div id="7"><p>7</p></div>
-            <div id="8"><p>8</p></div>
-            <div id="9"><p>9</p></div>
-            <div id="minus"><p>–</p></div>
-            <div id="4"><p>4</p></div>
-            <div id="5"><p>5</p></div>
-            <div id="6"><p>6</p></div>
-            <div id="plus"><p>+</p></div>
+          {/*<div id="ac">
+            <p>AC</p>
           </div>
+          <div id="divide"><p>/</p></div>
+          <div id="multiply"><p>x</p></div>
+          <div id="7"><p>7</p></div>
+          <div id="8"><p>8</p></div>
+          <div id="9"><p>9</p></div>
+          <div id="minus"><p>–</p></div>
+          <div id="4"><p>4</p></div>
+          <div id="5"><p>5</p></div>
+          <div id="6"><p>6</p></div>
+          <div id="plus"><p>+</p></div>*/}
         </div>
       </div>
     );
