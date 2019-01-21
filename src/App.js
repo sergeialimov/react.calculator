affe8fd856efe10e0bef6c3879ae17d03c1d231f
Sergei Alimov <8912652@gmail.com>
2019-01-21 12:33:06 +0300

    fix borders

--- src/App.js
+++ src/App.js
@@ -13,20 +13,20 @@ class App extends Component {
           </div>
           <div id="divide"><p>/</p></div>
           <div id="multiply"><p>x</p></div>
-          <div id="7"><p>7</p></div>
-          <div id="8"><p>8</p></div>
-          <div id="9"><p>9</p></div>
+          <div id="7" className="numbers"><p>7</p></div>
+          <div id="8" className="numbers"><p>8</p></div>
+          <div id="9" className="numbers"><p>9</p></div>
           <div id="minus"><p>–</p></div>
-          <div id="4"><p>4</p></div>
-          <div id="5"><p>5</p></div>
-          <div id="6"><p>6</p></div>
+          <div id="4" className="numbers"><p>4</p></div>
+          <div id="5" className="numbers"><p>5</p></div>
+          <div id="6" className="numbers"><p>6</p></div>
           <div id="plus"><p>+</p></div>
-          <div id="1"><p>1</p></div>
-          <div id="2"><p>2</p></div>
-          <div id="3"><p>3</p></div>
+          <div id="1" className="numbers"><p>1</p></div>
+          <div id="2" className="numbers"><p>2</p></div>
+          <div id="3" className="numbers"><p>3</p></div>
           <div id="equals"><p>=</p></div>
-          <div id="zero"><p>0</p></div>
-          <div id="dot"><p>.</p></div>
+          <div id="zero" className="numbers"><p>0</p></div>
+          <div id="dot" className="numbers"><p>.</p></div>
         </div>
       </div>
     );
