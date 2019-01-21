3de3666fe85337821f214c3ec84ee3efc7353322
Sergei Alimov <8912652@gmail.com>
2019-01-21 09:49:32 +0300

    add numbers

--- src/App.js
+++ src/App.js
@@ -13,14 +13,17 @@ class App extends Component {
           </div>
           <div id="divide"><p>/</p></div>
           <div id="multiply"><p>x</p></div>
-          {/*<div id="7"><p>7</p></div>
+          <div id="7"><p>7</p></div>
           <div id="8"><p>8</p></div>
           <div id="9"><p>9</p></div>
           <div id="minus"><p>–</p></div>
           <div id="4"><p>4</p></div>
           <div id="5"><p>5</p></div>
           <div id="6"><p>6</p></div>
-          <div id="plus"><p>+</p></div>*/}
+          <div id="plus"><p>+</p></div>
+          <div id="1"><p>1</p></div>
+          <div id="2"><p>2</p></div>
+          <div id="3"><p>3</p></div>
         </div>
       </div>
     );
